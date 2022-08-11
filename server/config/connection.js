19bb5b162ebb75c348747eb9ecbb1a9832d6d02a
Buney <dpkinser93@gmail.com>
2022-08-11 17:40:11 -0600

    added mongo connection to server

---
+++ server/config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/rpgdb-2',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    }
+);
+
+module.exports = mongoose.connection;
