d1f16e2a355d28e37164707e72428b14c346a597
Buney <dpkinser93@gmail.com>
2022-08-16 20:08:20 -0600

    added User typeDef

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,13 +2,13 @@
 const resolvers = {
     Query: {},
     Mutation: {
-        addUser: async (parent, { username, email, password }) => {
+        addUser: async (_parent, { username, email, password }) => {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
       
             return { token, user };
         },
-        login: async (parent, { email, password }) => {
+        login: async (_parent, { email, password }) => {
 
             const user = await User.findOne({ email: email });
       
