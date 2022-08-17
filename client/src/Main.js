2a7c22e112828535c3fa10e11beba6ceed19172d
Buney <dpkinser93@gmail.com>
2022-08-17 16:09:59 -0600

    added Home page

--- client/src/Main.js
+++ client/src/Main.js
@@ -3,13 +3,15 @@ import { Container } from 'react-bootstrap';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Nav from './components/Nav'
+import Home from './pages/Home'
 
 function Main() {
     return (
         <Container fluid>
             <Nav/>
             <Routes>
-                <Route path='/' element={<Login/>} />
+                <Route path='/' element={<Home/>} />
+                <Route path='login' element={<Login/>} />
             </Routes>
         </Container>
     )
