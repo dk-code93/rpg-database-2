e9bb436da9dd25c0604390d7aeae2045a570b946
Buney <dpkinser93@gmail.com>
2022-08-17 16:41:25 -0600

    added a username input for login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,14 +6,19 @@ function Login() {
     return (
         <Container>
             <Form>
+                <Form.Group className="mb-2" controlId="username">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type='username' placeholder="Enter a custom Username" />
+                </Form.Group>
+                
                 <Form.Group className="mb-2" controlId="email">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type='email' placeholder="Enter Email" />
+                    <Form.Control type='email' placeholder="Enter your email address" />
                 </Form.Group>
 
                 <Form.Group className="mb-2" controlId="password">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type='password' placeholder="Enter Password" />
+                    <Form.Control type='password' placeholder="Enter a password" />
                 </Form.Group>
 
                 <Button variant="dark" type='submit'>
