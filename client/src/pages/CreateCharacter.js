7273e7c90ca3777ae7693fd49742b281aac41dfb
Buney <dpkinser93@gmail.com>
2022-08-26 14:10:19 -0600

    added character create page

---
+++ client/src/pages/CreateCharacter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function CreateCharacter() {
+    return (
+        <Container>
+            
+        </Container>
+    )
+};
+
+export default CreateCharacter;
