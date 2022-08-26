3e6379f747837b76b4b9e8a999d67f04bb6fcd85
Buney <dpkinser93@gmail.com>
2022-08-26 14:59:38 -0600

    added form fields for Class and Level

--- client/src/pages/CreateCharacter.js
+++ client/src/pages/CreateCharacter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Row, Col } from 'react-bootstrap';
 
 function CreateCharacter() {
     return (
@@ -8,6 +8,28 @@ function CreateCharacter() {
                 <Form.Label>Name</Form.Label>
                 <Form.Control type='text' placeholder="Enter your character's name"/>
             </Form.Group>
+
+            <Row>
+                <Col>
+                    <Form.Group>
+                        <Form.Label>Class</Form.Label>
+                        <Form.Select id='class-select' placeholder='Select a class'>
+                            
+                        </Form.Select>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group>
+                        <Form.Label>Level</Form.Label>
+                        <Form.Control 
+                            type='number' 
+                            placeholder='1'
+                            min='1'
+                            />
+                    </Form.Group>
+                </Col>
+            </Row>
+            
         </Form>
     )
 };
