9d190e8ef6d3b1a6b393eda72734d01692ce3cd7
Buney <dpkinser93@gmail.com>
2022-08-26 15:28:22 -0600

    added state to character form

--- client/src/pages/CreateCharacter.js
+++ client/src/pages/CreateCharacter.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Row, Col } from 'react-bootstrap';
 import { classes } from '../utils/data';
 
 function CreateCharacter() {
+    const [formState, setFormState] = useState({
+        name: '',
+        level: 1,
+        class: '',
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+        console.log(value, formState)
+    }
+
     return (
         <Form>
             <Form.Group>
                 <Form.Label>Name</Form.Label>
-                <Form.Control type='text' placeholder="Enter your character's name"/>
+                <Form.Control 
+                    type='text' 
+                    name='name'
+                    placeholder="Enter your character's name"
+                    value={formState.name}
+                    onChange={handleChange}
+                />
             </Form.Group>
 
             <Row>
                 <Col>
                     <Form.Group>
                         <Form.Label>Class</Form.Label>
-                        <Form.Select id='class-select' placeholder='Select a class'>
-                            { classes.map( option => (
-                                <option key={option} value={option}>{option}</option>
+                        <Form.Select 
+                            id='class-select' 
+                            name='class'
+                            value={formState.class}
+                            onChange={handleChange}
+                        >
+                            { classes.map( classOption => (
+                                <option key={classOption} value={classOption}>{classOption}</option>
                                 ))
                             }
                         </Form.Select>
@@ -27,9 +53,12 @@ function CreateCharacter() {
                         <Form.Label>Level</Form.Label>
                         <Form.Control 
                             type='number' 
+                            name='level'
                             placeholder='1'
                             min='1'
-                            />
+                            value={formState.level}
+                            onChange={handleChange}
+                        />
                     </Form.Group>
                 </Col>
             </Row>
